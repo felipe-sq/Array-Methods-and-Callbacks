c546025cb8b24ad2b838f64dbc336c3c94ca4da3
Felipe Quintero <ffquint@gmail.com>
2020-10-31 14:25:12 -0400

    Completed Task 1 and Task 2.

--- index.js
+++ index.js
@@ -12,14 +12,25 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
+console.log(fifaData[850]["Home Team Name"]);
+console.log(fifaData[850]["Away Team Name"]);
+console.log(fifaData[850]["Home Team Goals"]);
+console.log(fifaData[850]["Away Team Goals"]);
+console.log(fifaData[850]["Away Team Name"]);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    const finalsArray = [];
+    for (let i = 0; i < fifaData.length; i++){
+        if (fifaData[i]["Stage"].includes(data)){
+            finalsArray.push(fifaData[i])};
+    }
+    return finalsArray;
 };
+
+
+console.log(getFinals("final"));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
